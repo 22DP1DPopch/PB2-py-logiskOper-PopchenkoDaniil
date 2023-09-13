2faac1845a9866287443b27b29ce9663154ab331
Daniel Popchenko <127397105+22DP1DPopch@users.noreply.github.com>
2023-09-13 14:02:26 +0300

    done 2 task

---
+++ 2piemers.py
@@ -0,0 +1,10 @@
+user_input = int(input('Enter coordinate: '))
+
+
+def define_insert(x):
+    if x >= 1:
+        print('Coordinate intersects')
+    else:
+        print('Coordinate does not intersect')
+
+define_insert(user_input)
